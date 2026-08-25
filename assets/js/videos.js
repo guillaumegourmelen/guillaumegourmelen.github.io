2367488dfa320b4bcd470b99ddf12a1483005965
Guillaume Gourmelen <guillaume.gourmelen@tutamail.com>
2026-08-25 12:24:44 +0900

    add news, arxiv link, kakenhi and talk at polyU

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -25,4 +25,5 @@ const VIDEO_LIBRARY = {
   'echo.demo':           '4J2LHxepwzU',
   'teleoperation.demo':  'bMBCkF2lrp8',
   'body-sublimation.demo': '4zXc3zHhKUU',
+  'wip.2026-07.g1-ik-spacemouse': 'WnSGyovosfc',
 };
